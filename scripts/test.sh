902280c139a05858bd84d2fefbcd68ee1c3afc09
Christian Menschel <christian@tapwork.de>
2014-09-29 18:30:52 +0200

    Changes build script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-xcodebuild -workspace XNGMarkdownParser.xcworkspace -scheme ExampleTests -sdk iphonesimulator clean build test | xcpretty -tc
+xcodebuild -workspace XNGMarkdownParser.xcworkspace -scheme ExampleTests -sdk iphonesimulator -destination platform='iOS Simulator',OS=7.1,name='iPhone Retina (4-inch)' clean build test | xcpretty -tc
