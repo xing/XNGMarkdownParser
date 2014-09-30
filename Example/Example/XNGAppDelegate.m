f1c288d9b0581fca38b72b895f7f9ddb60b434d6
Christian Menschel <christian@tapwork.de>
2014-09-30 10:58:24 +0200

    Does not start the rootViewController when running tests

--- Example/Example/XNGAppDelegate.m
+++ Example/Example/XNGAppDelegate.m
@@ -6,11 +6,32 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:UIScreen.mainScreen.bounds];
     self.window.backgroundColor = [UIColor whiteColor];
-
+    
+    if ([[self class] isRunningUnitTests]) {
+        return YES;
+    }
+    
     XNGMarkdownTestViewController *viewController = [[XNGMarkdownTestViewController alloc] init];
     self.window.rootViewController = viewController;
     [self.window makeKeyAndVisible];
+    
     return YES;
+}
+
+static BOOL RunningTests = NO;
+
++ (BOOL)isRunningUnitTests {
+    static dispatch_once_t oncePredicate;
+    dispatch_once(&oncePredicate, ^{
+        NSString *XCInjectBundle = [[[NSProcessInfo processInfo] environment] objectForKey:@"XCInjectBundle"];
+        
+        RunningTests = [XCInjectBundle hasSuffix:@".xctest"];
+        if ([XCInjectBundle.lastPathComponent isEqualToString:@"KIFTests.xctest"]) {
+            RunningTests = NO;
+        }
+    });
+    
+    return RunningTests;
 }
 
 
