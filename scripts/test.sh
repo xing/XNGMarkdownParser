1c78d75aff223b99c740e0a5222757214536ef37
Christian Menschel <christian@tapwork.de>
2014-09-30 11:02:12 +0200

    Removes build shell script / we moved to rake task

--- scripts/test.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-xcodebuild -workspace XNGMarkdownParser.xcworkspace -scheme ExampleTests -sdk iphonesimulator -destination platform='iOS Simulator',OS=7.1,name='iPhone Retina (4-inch)' clean build test | xcpretty -tc
