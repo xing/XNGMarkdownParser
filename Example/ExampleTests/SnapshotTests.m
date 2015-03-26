efc950fad2f6bd595685e1572ad900438d66e44a
Silvan Dähn <silvan.daehn@xing.com>
2015-03-26 17:39:27 +0100

    Unify code style in tests

--- Example/ExampleTests/SnapshotTests.m
+++ Example/ExampleTests/SnapshotTests.m
@@ -8,7 +8,7 @@
 
 @implementation ExampleTests
 
-- (void) setUp {
+- (void)setUp {
     [super setUp];
     self.recordMode = NO;
 }
@@ -32,32 +32,31 @@
 - (void)testFontChange {
     NSString *markdown = [self markdownFromFile:@"all_together_short.txt"];
 
-    XNGMarkdownParser * parser = [[XNGMarkdownParser alloc] init];
+    XNGMarkdownParser *parser = [[XNGMarkdownParser alloc] init];
     parser.paragraphFont = [UIFont fontWithName:@"Damascus" size:15];
     parser.codeFontName = @"Menlo-Regular";
     parser.boldFontName = @"EuphemiaUCAS-Bold";
     parser.linkFontName = @"Futura-Medium";
     [parser setFont:[UIFont fontWithName:@"Copperplate" size:24]
           forHeader:XNGMarkdownParserHeader1];
-    NSAttributedString * attr = [parser attributedStringFromMarkdownString:markdown];
+    NSAttributedString *attr = [parser attributedStringFromMarkdownString:markdown];
 
     FBSnapshotVerifyView([self defaultLabelWithAttributedString:attr], nil);
 }
 
 - (void)testParagraphAttributes {
     UILabel *label = [self defaultTextLabel];
-    NSString * markdown = [self markdownFromFile:@"paragraph_1.txt"];
+    NSString *markdown = [self markdownFromFile:@"paragraph_1.txt"];
 
     XNGMarkdownParser *parser = [[XNGMarkdownParser alloc] init];
     NSShadow *shadow = [[NSShadow alloc] init];
     shadow.shadowOffset = CGSizeMake(0.5, 0.5);
     shadow.shadowBlurRadius = 0.5;
-    NSMutableParagraphStyle * paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+    NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
     paragraphStyle.lineSpacing = 6;
-    parser.topAttributes = @{NSForegroundColorAttributeName : [UIColor darkGrayColor],
-                             NSShadowAttributeName : shadow,
-                             NSParagraphStyleAttributeName : paragraphStyle,
-                             };
+    parser.topAttributes = @{NSForegroundColorAttributeName: [UIColor darkGrayColor],
+                             NSShadowAttributeName: shadow,
+                             NSParagraphStyleAttributeName: paragraphStyle};
     label.attributedText = [parser attributedStringFromMarkdownString:markdown];
 
     FBSnapshotVerifyView(label, nil);
@@ -65,23 +64,22 @@
 
 #pragma mark - Helper methods
 
-- (UILabel*)labelForMarkdownStringWithDefaultAttributesFromFile:(NSString*)fileName {
+- (UILabel *)labelForMarkdownStringWithDefaultAttributesFromFile:(NSString *)fileName {
     UILabel *label = [self defaultTextLabel];
-    NSAttributedString * attrString = [self parseWithDefaultAttributes:[self markdownFromFile:fileName]];
+    NSAttributedString *attrString = [self parseWithDefaultAttributes:[self markdownFromFile:fileName]];
     [self setAttributedTextAndResizeAutomatically:attrString
                                           inLabel:label];
     return label;
 }
 
-- (UILabel*)defaultLabelWithAttributedString:(NSAttributedString*)attr {
+- (UILabel *)defaultLabelWithAttributedString:(NSAttributedString *)attr {
     UILabel *label = [self defaultTextLabel];
     [self setAttributedTextAndResizeAutomatically:attr inLabel:label];
     return label;
 }
 
-- (void)setAttributedTextAndResizeAutomatically:(NSAttributedString*)attrString
-                                        inLabel:(UILabel*)view
-{
+- (void)setAttributedTextAndResizeAutomatically:(NSAttributedString *)attrString
+                                        inLabel:(UILabel *)view {
     view.attributedText = attrString;
     CGSize size = [view sizeThatFits:CGSizeMake(320, CGFLOAT_MAX)];
     view.frame = CGRectMake(0, 0, size.width, size.height);
@@ -97,14 +95,13 @@
 }
 
 - (NSString *)markdownFromFile:(NSString *)name {
-    NSBundle * b = [NSBundle bundleForClass:self.class];
-    NSString *file = [b pathForResource:name ofType:nil];
-
-    NSString *markdown = [[NSString alloc] initWithContentsOfFile:file encoding:NSUTF8StringEncoding error:nil];
+    NSBundle *bundle = [NSBundle bundleForClass:self.class];
+    NSString *path = [bundle pathForResource:name ofType:nil];
+    NSString *markdown = [[NSString alloc] initWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
     return markdown;
 }
 
-- (NSAttributedString *)parseWithDefaultAttributes:(NSString*)markdown {
+- (NSAttributedString *)parseWithDefaultAttributes:(NSString *)markdown {
     XNGMarkdownParser *parser = [[XNGMarkdownParser alloc] init];
     return [parser attributedStringFromMarkdownString:markdown];
 }
